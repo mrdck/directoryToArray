7c2ec34e6d753c9dccc8e18d86d5df44b8accd70
Mateusz Perlak <mrdck@icloud.com>
2019-02-21 13:54:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-const fs = require('fs');
-const path = require('path');
-
-
+const fs = require('fs')
+const path = require('path')
 
 const dirToArray = (dir) => {
 	return [...fs.readdirSync(dir).filter( file =>  file !== '.DS_Store')] 
 }
-
 
 module.exports = dirToArray
